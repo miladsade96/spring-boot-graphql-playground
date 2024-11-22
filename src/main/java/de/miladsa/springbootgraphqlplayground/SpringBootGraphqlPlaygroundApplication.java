7ef84765f3d292e8bb410657fc99df472bd075b4
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-22 15:09:39 +0330

    Initial commit

---
+++ src/main/java/de/miladsa/springbootgraphqlplayground/SpringBootGraphqlPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package de.miladsa.springbootgraphqlplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGraphqlPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/miladsa/springbootgraphqlplayground/SpringBootGraphqlPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package de.miladsa.springbootgraphqlplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphqlPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
